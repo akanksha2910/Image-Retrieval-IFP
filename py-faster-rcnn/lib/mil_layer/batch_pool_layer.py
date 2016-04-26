9057b3ddbe437f3ed9c618624079001183fe9d8b
Xianming Liu <liuxianming@gmail.com>
2016-04-25 22:48:54 -0500

    fix bugs in batch pooling layer

--- py-faster-rcnn/lib/mil_layer/batch_pool_layer.py
+++ py-faster-rcnn/lib/mil_layer/batch_pool_layer.py
@@ -45,7 +45,7 @@ class BatchPoolLayer(caffe.Layer):
         if self._pooling == 'AVE':
             # perform average pooling
             pooled_data_ = np.mean(bottom_data_, axis=0)
-        top[0].data[...] = pooled_data_.reshape(1, bottom[0].shpae[1:])
+        top[0].data[0, ...] = pooled_data_.reshape(bottom[0].data.shape[1:])
 
     def backward(self, top, propagate_down, bottom):
         """Calculate backward gradients
@@ -55,18 +55,18 @@ class BatchPoolLayer(caffe.Layer):
         if self._pooling == 'MAX':
             if self._max_ids is not None:
                 mask_ = self._max_ids.flatten()
-                bottom_diff_ = np.zeros(bottom[0].shape)
+                bottom_diff_ = np.zeros(bottom[0].diff.shape)
                 bottom_diff_ = bottom_diff_.reshape(num_, -1)
                 for i in range(len(top_diff_)):
                     pos = mask_[i]
-                    bottom_diff_[i, pos] = top_diff_[i]
+                    bottom_diff_[pos, i] = top_diff_[i]
             else:
                 print("Max pooling mask is empty")
                 raise
         if self._pooling == 'AVE':
             # For average pooling, just pass down the top gradient
             bottom_diff_ = top_diff_[np.newaxis, :] / num_
-        bottom[0].diff[...] += bottom_diff_.reshape(bottom[0].shape)
+        bottom[0].diff[...] += bottom_diff_.reshape(bottom[0].diff.shape)
 
     def reshape(self, bottom, top):
         top[0].reshape(1, *(bottom[0].shape[1:]))
